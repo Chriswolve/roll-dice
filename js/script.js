01943d5d291b53e8c242261afca0aecd47a27620
Christian <christian@iMac-de-Wilson.local>
2021-12-09 16:23:11 -0500

    base commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Utilizando LET
+console.log("Antes, ", p);
+//Para (Iniciar iterador ; iterador < total ; iterador++)
+for (let p = 0; p < 3; p++) {
+  console.log("- ", p);
+}
+console.log("Despues p :", p);
+
+// Utilizando VAR
+console.log("Antes, ", p);
+for (var p = 0; p < 3; p++) {
+  console.log("- ", p);
+}
+console.log("Despues p :", p);
